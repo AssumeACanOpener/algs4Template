8574c663f71545f6fa891e2a2dc9194a401e7498
Glen Kaukola <gkaukola@cs.ucr.edu>
2014-06-26 23:39:29 -0700

    Added some actual ThreeSum tests

--- src/test/java/ThreeSumTest.java
+++ src/test/java/ThreeSumTest.java
@@ -8,13 +8,8 @@ import static org.junit.Assert.fail;
 
 public class ThreeSumTest {
 
-  int MAX = 3;
-  int[] a = new int[N];
-  for (int i = 0; i < N; i++) {
-    a[i] = StdRandom.uniform(-MAX, MAX);
-  }
-  public ThreeSum testThreeSum = new ThreeSum(a);
-  cnt = testThreeSum.count();
+  public int MAX = 3;
+  public ThreeSum testThreeSum = new ThreeSum();
 
   @Test
   public void thisAlwaysPasses() {
@@ -26,15 +21,24 @@ public class ThreeSumTest {
   }
 
   @Test
-  public void examples() {
+  public void assertExamples() {
     assertTrue("Test 1 failure: should be true.", true);
     assertFalse("Test 2 failure: should be false.", false);
     assertEquals("Test 3 failure: 0 should equal 0.", 0, 0);
   }
 
   @Test
-  public void basicThreeSum() {
+  public void simpleThreeSum() {
+    // Generate some data to feed to ThreeSum.count()
+    int[] a = new int[MAX];
+    for (int i = 0; i < MAX; i++) {
+      a[i] = StdRandom.uniform(-MAX, MAX);
+    }
+
+    int cnt = testThreeSum.count(a);
+    // This should always be true
     assertTrue("Test 1 failure: count should never be negative.", (cnt >= 0));
+    // If MAX == 3 then this should always be true
     assertTrue("Test 2 failure: count should never be more than one.", (cnt < 2));
   }
 
